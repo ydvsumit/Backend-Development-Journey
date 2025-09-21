322e082ddb9a2d190898d313171f512764bc5e68
Sumit Yadav <iamsumitydv@gmail.com>
2025-09-22 01:55:19 +0530

    feat: implemented lodash

--- NodeJS/1-Fundamentals/5-NPM/index.js
+++ NodeJS/1-Fundamentals/5-NPM/index.js
@@ -26,7 +26,7 @@ console.log(newItems);
 
 /**
  * package.json :
- * nodemon: It is a toll for nodejs that gonna watch our files and restart our application automatically whenever you make changes to your code.
+ * nodemon: It is a tool for nodejs that gonna watch our files and restart our application automatically whenever you make changes to your code.
  * dependencies: These are the packages your app need to run in production.
  *             - They're required when the app is actually running.
  *             - If you deploy your app to a server, these packages will be installed.
@@ -40,3 +40,39 @@ console.log(newItems);
  * Uninstall Packages: you can uninstall any package that you want.
  * Syntax: npm uninstall <packageName>
  */
+
+/**
+ * ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ * What is Lodash?
+ *                - Lodash is a popular utility library in Node.js (and JavaScript in general).
+ *                - It gives you a big collection of ready-made helper functions that make it easier to work with:
+ *                  • Arrays
+ *                  • Objects
+ *                  • Strings
+ *                  • Numbers
+ *                  • Collections
+ * In plain JavaScript, some operations are long or repetitive. Lodash makes them shorter, cleaner, and safer.
+ */
+
+// 1. Finding the maximum number
+// Without Lodash
+const numbers = [3, 6, 2, 8, 4];
+const max = Math.max(...numbers);
+console.log(max); // 8
+
+// With Lodash
+console.log(_.max(numbers)); // 8
+
+// 2. Deep cloning an object
+// Without Lodash (tricky way)
+const obj = { a: 1, b: { c: 2 } };
+const clone = JSON.parse(JSON.stringify(obj));
+console.log("without loadash - clone: ", clone);
+
+// With Lodash
+const clone2 = _.cloneDeep(obj);
+console.log("With loadash - clone: ", clone2);
+
+// 3. Remove duplicates from array
+const nums = [1, 2, 2, 3, 4, 4, 5];
+console.log(_.uniq(nums)); // [1, 2, 3, 4, 5]
