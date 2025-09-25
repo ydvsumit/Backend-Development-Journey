369c19357560511ebd8985650bfe2b83e4283eb8
Sumit Yadav <iamsumitydv@gmail.com>
2025-09-26 02:00:31 +0530

    feat: added info of readLine

--- NodeJS/1-Fundamentals/1-2-Basic-Start/5-readLine.js
+++ NodeJS/1-Fundamentals/1-2-Basic-Start/5-readLine.js
@@ -1,3 +1,15 @@
+/**
+ * Accept input from the command line in Node.js
+ *  ■ How to make a Node.js CLI program interactive?
+ *    - Node.js since version 7 provides the readline module to perform exactly this: get input from a readable stream such as the process.stdin stream,
+ *      which during the execution of a Node.js program is the terminal input, one line at a time.
+ *
+ * How it works?
+ * • This piece of code asks the user's name, and once the text is entered and the user presses enter, we send a greeting.
+ * • The question() method shows the first parameter (a question) and waits for the user input. It calls the callback function once enter is pressed.
+ * • In this callback function, we close the readline interface.
+ * • readline offers several other methods, please check them out on the package documentation linked above.
+ */
 const readline = require("node:readline");
 
 const rl = readline.createInterface({
