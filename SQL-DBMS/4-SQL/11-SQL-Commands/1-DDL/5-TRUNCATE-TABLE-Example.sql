846d1c27e355e7bf574e51cd3ccfb478d7ded213
Sumit Yadav <iamsumitydv@gmail.com>
2025-11-03 18:13:55 +0530

    feat: added TRUNCATE and SQL Constraints

---
+++ SQL-DBMS/4-SQL/11-SQL-Commands/1-DDL/5-TRUNCATE-TABLE-Example.sql
@@ -0,0 +1,5 @@
+-- DROP TABLE TABLE_NAME will delete the table permanently
+-- DROP TABLE courses
+
+-- TRUNCATE -> TRUNCATE quickly empties a table but keeps its structure for future use
+TRUNCATE TABLE students;
