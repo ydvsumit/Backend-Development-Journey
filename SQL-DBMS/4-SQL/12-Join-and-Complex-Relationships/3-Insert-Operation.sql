660c0c483ea97d27eed3fd1f711ed82fae73afc9
Sumit Yadav <iamsumitydv@gmail.com>
2026-04-07 07:54:07 +0530

    feat: added insert operation in join and complext relationship

---
+++ SQL-DBMS/4-SQL/12-Join-and-Complex-Relationships/3-Insert-Operation.sql
@@ -0,0 +1,10 @@
+INSERT INTO classes VALUES
+(101, 'Math', 'Mr. Sharma'),
+(102, 'Physics', 'Dr. Verma'),
+(103, 'Chemistry', 'Mr. Kapoor');
+
+INSERT INTO enrollments VALUES
+(1, 1, 101),
+(2, 1, 102),
+(3, 2, 103),
+(4, 3, 101);
